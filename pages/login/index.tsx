bea17c7bc23dc93ce105bb0b1efd80549b6a23b0
kkamang <joywithjoy1203@gmail.com>
2024-02-02 13:25:18 +0900

    login/index.tsx 공백 수정

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -26,29 +26,31 @@ export default function Signin() {
   }
 
   return (
-    <div className="auth-page">
-  <div className="container page">
-    <div className="row">
-      <div className="col-md-6 offset-md-3 col-xs-12">
-        <h1 className="text-xs-center">Sign in</h1>
-        <p className="text-xs-center">
-          <Link href="/register">Need an account?</Link>
-        </p>
+    <>
+      <div className="auth-page">
+        <div className="container page">
+          <div className="row">
+            <div className="col-md-6 offset-md-3 col-xs-12">
+              <h1 className="text-xs-center">Sign in</h1>
+              <p className="text-xs-center">
+                <Link href="/register">Need an account?</Link>
+              </p>
 
-        <AuthErrors errors={errors} />
+              <AuthErrors errors={errors} />
 
-        <form>
-          <fieldset className="form-group">
-            <input className="form-control form-control-lg" type="text" placeholder="Email" />
-          </fieldset>
-          <fieldset className="form-group">
-            <input className="form-control form-control-lg" type="password" placeholder="Password" />
-          </fieldset>
-          <button className="btn btn-lg btn-primary pull-xs-right">Sign in</button>
-        </form>
+              <form>
+                <fieldset className="form-group">
+                  <input className="form-control form-control-lg" type="text" placeholder="Email" />
+                </fieldset>
+                <fieldset className="form-group">
+                  <input className="form-control form-control-lg" type="password" placeholder="Password" />
+                </fieldset>
+                <button className="btn btn-lg btn-primary pull-xs-right">Sign in</button>
+              </form>
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
-  </div>
-</div>
+    </>
   );
 }
